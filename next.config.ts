b3feb6454f70b7663f3796bf0161a9adeff42c08
Vinay <vinayvarma10101998@gmail.com>
2025-11-16 18:59:13 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "192.168.0.104"
+  ]
 };
 
 export default nextConfig;
